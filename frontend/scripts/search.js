65c3608fddea79a40099971130b8db82f7f9a6ce
vinniciusJ <vinnicius.jimenez@gmail.com>
2020-04-20 12:12:20 -0300

    Add users informations

--- frontend/scripts/search.js
+++ frontend/scripts/search.js
@@ -1,6 +1,7 @@
-const section = document.querySelector('section')
-const ul = document.querySelector('ul')
+const section = document.querySelector('.sec')
+const ul = document.querySelector('.list')
 const liSearch = document.querySelector('.search')
+
 
 const filterUsers = async (name) =>
     fetch(`https://jsonplaceholder.typicode.com/users?name_like=${name}`)
@@ -10,18 +11,11 @@ const filterUsers = async (name) =>
 const debounceEvent = (fn, wait = 200, time) =>  (...args) =>
     clearTimeout(time, time = setTimeout(() => fn(...args), wait))
     
-function openiFrame(){
-    const iFrame = document.createElement('iframe')
-    
-    iFrame.src = 'http://www.google.com'
-    document.appendChild(iFrame)
-    
-}
 
 function handleKeyUp(event) {
     let usersList = []
 
-    const name = String(event.target.value)
+    let name = String(event.target.value)
 
     if(!notNull(name)){
         ul.innerHTML = ''
@@ -30,7 +24,9 @@ function handleKeyUp(event) {
 
         return
     }
-
+    if(name == '*'){
+        name = ' '
+    }
 
     filterUsers(name)
     .then(users => {
@@ -44,10 +40,52 @@ function handleKeyUp(event) {
             img.style.height = '16px'
 
             button.append(img)
-            button.onclick = 'openiFrame()'
 
             li.className = 'res'
-            li.append(user.name, button)
+            li.append(user.name)
+
+            button.addEventListener('click', () =>{
+                const container = document.querySelector('.container')
+                const userContainer = document.querySelector('.user-container')
+                
+                const ulInformation = document.querySelector('.res-information-user')
+
+                container.style.display = 'none'
+                userContainer.style.display = 'initial'
+
+                const buttonClose = document.querySelector('.close')
+                const imagemClose = document.querySelector('.close img')
+                
+                const datas = ['name', 'username', 'email', 'website', 'phone']
+                let datasList = []
+
+                datas.map(type => {
+                    const liData = document.querySelector(`#information-${type} span`)
+                    liData.textContent = user[type]
+                    datasList.push(liData)
+                })
+
+                datasList.map(liData => {
+                    ulInformation.append(liData)
+                })
+
+                imagemClose.addEventListener('mouseover', () => {
+                    imagemClose.src = '../frontend/images/x-circle-close.svg'
+                })
+                
+                buttonClose.addEventListener('click', () => {
+                    
+                    for(let i = 0; i < datasList.length; i++){
+                        datasList[i].textContent = ''
+                    }
+                    
+                    container.style.display = 'grid'
+                    userContainer.style.display = 'none'
+                })
+                
+            })
+
+            li.append(button)
             
             if(usersList.length === 0){
                 usersList.push(li)
@@ -66,7 +104,7 @@ function handleKeyUp(event) {
             ul.appendChild(user)
         })
     })
-    ul.innerHTML = ''
+    ul.innerHTML = ' '
 }
 function notNull(str){
     if(str != '' && !(str.match(/^\s+$/))){
@@ -77,8 +115,10 @@ function notNull(str){
     }
 }
 
+
 document.querySelector("input")
 .addEventListener("keyup", debounceEvent(handleKeyUp, 500))
+
 
 
 
